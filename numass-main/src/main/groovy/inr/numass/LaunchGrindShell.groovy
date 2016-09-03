2cfe2e45e3f63455583f2f1fe8b4a1177a64367a
Alexander Nozik <altavir@gmail.com>
2016-09-03 22:26:18 +0300

    Grind shell refactoring

--- numass-main/src/main/groovy/inr/numass/LaunchGrindShell.groovy
+++ numass-main/src/main/groovy/inr/numass/LaunchGrindShell.groovy
@@ -1,12 +1,14 @@
 package inr.numass
 
+import hep.dataforge.grind.GrindLauncher
 import hep.dataforge.grind.GrindShell
+import hep.dataforge.workspace.Workspace
 
 /**
  * Created by darksnake on 29-Aug-16.
  */
 
 GrindShell shell = new GrindShell()
-shell.launcher.withSpec(NumassWorkspaceSpec)
-shell.launcher.from(new File("D:\\Work\\Numass\\sterile2016\\workspace.groovy"))
+Workspace numass = new GrindLauncher().withSpec(NumassWorkspaceSpec).from(new File("D:\\Work\\Numass\\sterile2016\\workspace.groovy")).buildWorkspace()
+shell.bind("numass", numass)
 shell.start()
