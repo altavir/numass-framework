171a4f76e80566e4f60f1bc22ff6f39a0f3f17b7
darksnake <darksnake@LUNA.localdomain>
2016-11-04 20:52:22 +0300

    minor fixes

--- numass-main/src/main/groovy/inr/numass/scripts/Underflow.groovy
+++ numass-main/src/main/groovy/inr/numass/scripts/Underflow.groovy
@@ -14,13 +14,13 @@ import inr.numass.utils.UnderflowCorrection
 
 //File dataDir = new File("D:\\Work\\Numass\\data\\2016_04\\T2_data\\Fill_2_2\\set_7_b2a3433e54010000")
 //File dataDir = new File("D:\\Work\\Numass\\data\\2016_04\\T2_data\\Fill_2_2\\set_6_e26d123e54010000")
-File dataDir = new File("D:\\Work\\Numass\\data\\2016_10\\Fill_2_wide\\\\set_21")
+File dataDir = new File("D:\\Work\\Numass\\data\\2016_10\\Fill_1\\set_26")
 if(!dataDir.exists()){
     println "dataDir directory does not exist"
 }
 NumassData data = NumassDataLoader.fromLocalDir(null, dataDir)
 //NumassData data = NMFile.readFile(new File("D:\\Work\\Numass\\sterilie2013-2014\\dat\\2013\\SCAN06.DAT" ))
 
-Table t = new UnderflowCorrection().fitAllPoints(data, 500, 1000, 1800, 20);
+Table t = new UnderflowCorrection().fitAllPoints(data, 400, 650, 1800, 20);
 ColumnedDataWriter.writeDataSet(System.out, t, "underflow parameters")
 
