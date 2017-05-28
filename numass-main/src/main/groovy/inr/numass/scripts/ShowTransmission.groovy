d86a6c46c78f608d77cec7c1bb44db8412d55491
Alexander Nozik <altavir@gmail.com>
2017-05-28 19:46:57 +0300

    Numass control room global update

--- numass-main/src/main/groovy/inr/numass/scripts/ShowTransmission.groovy
+++ numass-main/src/main/groovy/inr/numass/scripts/ShowTransmission.groovy
@@ -4,7 +4,6 @@ import hep.dataforge.context.Context
 import hep.dataforge.context.Global
 import hep.dataforge.grind.Grind
 import hep.dataforge.grind.helpers.PlotHelper
-import hep.dataforge.meta.Meta
 import hep.dataforge.plots.fx.FXPlotManager
 import inr.numass.models.sterile.NumassResolution
 import javafx.application.Platform
@@ -12,14 +11,29 @@ import javafx.application.Platform
 Context ctx = Global.instance()
 ctx.pluginManager().load(FXPlotManager)
 
-Meta meta = Grind.buildMeta("resolution", width: 8.3e-5, tail: "(0.99797 - 3.05346E-7*D - 5.45738E-10 * D**2 - 6.36105E-14 * D**3)")
-
 PlotHelper plot = new PlotHelper(ctx);
 
-NumassResolution resolution = new NumassResolution(ctx, meta)
+def resolution1 = new NumassResolution(
+        ctx,
+        Grind.buildMeta("resolution", width: 8.3e-5, tail: "(0.99797 - 3.05346E-7*D - 5.45738E-10 * D**2 - 6.36105E-14 * D**3)")
+)
 
 plot.plot(from: 13500, to: 19000) { x ->
-    resolution.value(18500, x, null)
+    resolution1.value(x, 14000, null)
 }
+
+def resolution2 = new NumassResolution(
+        ctx,
+        Grind.buildMeta("resolution",
+                width: 8.3e-5,
+                tail: "(0.99797 - 3.05346E-7*D - 5.45738E-10 * D**2 - 6.36105E-14 * D**3)*(1-5e-3*sqrt(E/1000))"
+        )
+)
+
+plot.plot(from: 13500, to: 19000) { x ->
+    resolution2.value(x, 14000, null)
+}
+
+
 Platform.setImplicitExit(true)
 
