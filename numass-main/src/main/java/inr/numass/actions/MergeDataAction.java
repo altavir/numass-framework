789e8b04c8e2138bcf239033107e83c653d4f708
Alexander Nozik <altavir@gmail.com>
2017-08-30 19:05:58 +0300

    Update in data loaders and exec action

--- numass-main/src/main/java/inr/numass/actions/MergeDataAction.java
+++ numass-main/src/main/java/inr/numass/actions/MergeDataAction.java
@@ -35,7 +35,7 @@ import java.util.*;
  * @author Darksnake
  */
 @TypedActionDef(name = "numass.merge", inputType = Table.class, outputType = Table.class, info = "Merge different numass data files into one.")
-@NodeDef(name = "grouping", info = "The definition of grouping rule for this merge", target = "method::hep.dataforge.actions.GroupBuilder.byMeta")
+@NodeDef(name = "grouping", info = "The definition of grouping rule for this merge", from = "method::hep.dataforge.actions.GroupBuilder.byMeta")
 public class MergeDataAction extends ManyToOneAction<Table, Table> {
 
     public static final String MERGE_NAME = "mergeName";
