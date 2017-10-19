72b046abfeb73377168aa1c7eadce7b4f8050f30
Alexander Nozik <altavir@gmail.com>
2017-10-19 22:04:42 +0300

    Numass tasks fix

--- numass-main/src/main/kotlin/inr/numass/tasks/NumassTasks.kt
+++ numass-main/src/main/kotlin/inr/numass/tasks/NumassTasks.kt
@@ -8,13 +8,20 @@ import inr.numass.data.analyzers.SmartAnalyzer
 import inr.numass.data.api.NumassSet
 import inr.numass.utils.NumassUtils
 
-val selectDataTask = task("data") {
+val selectDataTask = task("select") {
+    model {
+        data("*")
+        configure(meta.getMetaOrEmpty("data"))
+    }
     transform { data ->
         CustomDataFilter(meta).filter<NumassSet>(data.checked(NumassSet::class.java))
     }
 }
 
 val monitorTableTask = task("monitor") {
+    model {
+        dependsOn("select", meta)
+    }
     join<NumassSet, Table> {
         result { data ->
             val monitorVoltage = meta.getDouble("monitorVoltage", 16000.0);
@@ -28,7 +35,7 @@ val monitorTableTask = task("monitor") {
                                     .map { it -> analyzer.analyzePoint(it, analyzerMeta) }
                     )
 
-            context.io().out("numass.monitor",name).use {
+            context.io().out("numass.monitor", name).use {
                 NumassUtils.write(it, meta, builder.build())
             }
 
