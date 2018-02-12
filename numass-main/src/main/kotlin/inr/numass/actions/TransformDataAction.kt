9a2bf6350fa31e7826eaebe0298fe9e8a152513e
Alexander Nozik <altavir@gmail.com>
2018-02-12 15:19:17 +0300

    Fixed table metamorphing

--- numass-main/src/main/kotlin/inr/numass/actions/TransformDataAction.kt
+++ numass-main/src/main/kotlin/inr/numass/actions/TransformDataAction.kt
@@ -99,7 +99,7 @@ class TransformDataAction : OneToOneAction<Table, Table>() {
         val res = table.addColumn(ListColumn.build(table.getColumn(COUNT_RATE_KEY).format, cr.stream()))
                 .addColumn(ListColumn.build(table.getColumn(COUNT_RATE_ERROR_KEY).format, crErr.stream()))
 
-        context.io.output(name, name).push(NumassUtils.wrap(res, meta))
+        context.io.output(name, getName()).push(NumassUtils.wrap(res, meta))
         return res
     }
 
